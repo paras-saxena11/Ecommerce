3f9a0f6ebacdd2b13102b6c5436fa6e7add5f79e
paras-saxena11 <77022754+paras-saxena11@users.noreply.github.com>
2023-03-22 21:23:48 +0530

    Uploading to github

---
+++ backend/connection.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const DB_url = "mongodb://localhost:27017/ecom";
+mongoose.connect(DB_url, { useNewUrlParser: true }).then(() => console.log("Db connected")).catch(err => console.log(err));
