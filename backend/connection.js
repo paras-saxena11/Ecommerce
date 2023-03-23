5f14e990d04589e4d44d13effb74e3ff5cf02160
paras-saxena11 <77022754+paras-saxena11@users.noreply.github.com>
2023-03-23 20:39:53 +0530

    updating conn

--- backend/connection.js
+++ backend/connection.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+const DB_URL = process.env.DB_URL;
 
 const DB_url = "mongodb://localhost:27017/ecom";
 mongoose.connect(DB_url, { useNewUrlParser: true }).then(() => console.log("Db connected")).catch(err => console.log(err));
--- backend/server.js
+++ backend/server.js
@@ -4,48 +4,47 @@ const app = express();
 const http = require('http');
 require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
-require('./connection')
+const PORT = process.env.PORT;
+require("./connection");
 const server = http.createServer(app);
-const {Server} = require('socket.io');
+const { Server } = require("socket.io");
 const io = new Server(server, {
-  cors: 'http://localhost:3000',
-  methods: ['GET', 'POST', 'PATCH', "DELETE"]
-})
+  cors: "http://localhost:3000",
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+});
 
-const User = require('./models/User');
-const userRoutes = require('./routes/userRoutes');
-const imageRoutes = require('./routes/imageRoutes');
-const productRoutes = require('./routes/productRoutes');
-const orderRoutes = require('./routes/orderRoutes');
+const User = require("./models/User");
+const userRoutes = require("./routes/userRoutes");
+const imageRoutes = require("./routes/imageRoutes");
+const productRoutes = require("./routes/productRoutes");
+const orderRoutes = require("./routes/orderRoutes");
 
 app.use(cors());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/users', userRoutes);
-app.use('/products', productRoutes);
-app.use('/images', imageRoutes);
-app.use('/orders', orderRoutes);
+app.use("/users", userRoutes);
+app.use("/products", productRoutes);
+app.use("/images", imageRoutes);
+app.use("/orders", orderRoutes);
 
-
-app.post('/create-payment', async(req, res)=> {
-  const {amount} = req.body;
+app.post("/create-payment", async (req, res) => {
+  const { amount } = req.body;
   // console.log(amount);
   try {
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
-      currency: 'usd',
-      payment_method_types: ['card']
+      currency: "usd",
+      payment_method_types: ["card"],
     });
-    res.status(200).json(paymentIntent)
+    res.status(200).json(paymentIntent);
   } catch (e) {
     console.log(e.message);
     res.status(400).json(e.message);
-   }
-})
+  }
+});
 
-
-server.listen(8080, ()=> {
-  console.log('server running at port', 8080)
-})
+server.listen(PORT, () => {
+  console.log("server running at port", 8080);
+});
 
 app.set('socketio', io);
